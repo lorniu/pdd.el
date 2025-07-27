15594c3348ed7febef31f99d709b128f267035eb
lorniu <lorniu@gmail.com>
2025-07-27 08:45:52 +0800

    Bump version to v0.2.3.

--- pdd.el
+++ pdd.el
@@ -6,7 +6,7 @@
 ;; URL: https://github.com/lorniu/pdd.el
 ;; License: GPL-3.0-or-later
 ;; Package-Requires: ((emacs "28.1"))
-;; Version: 0.2.2
+;; Version: 0.2.3
 
 ;; This file is not part of GNU Emacs.
 
